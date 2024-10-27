8459056873ed17ec087faf5b43b044449b5c4e09
paula raafat <paularaafat27@.com>
2024-10-27 15:35:00 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,8 @@
 """
 0-simple_helper_function.py file
 """
+
+
 def index_range(page, page_size):
     """function to return the start index and
     the end index of dataset
@@ -14,4 +16,4 @@ def index_range(page, page_size):
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return  (start_index, end_index)
+    return (start_index, end_index)
