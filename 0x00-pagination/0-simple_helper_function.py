33f08c8ab33ec780f367a474c9347897bb8bef2e
paula raafat <paularaafat27@.com>
2024-10-27 15:32:22 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,5 +14,4 @@ def index_range(page, page_size):
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    result = (start_index, end_index)
-    return result
+    return  (start_index, end_index)
