636de9fda5d70bc22b4227f00ad443523b515871
paula raafat <paularaafat27@.com>
2024-10-28 22:15:21 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,7 +6,7 @@
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 def index_range(page, page_size):
@@ -48,18 +48,18 @@ class Server:
 
         return dataset[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Retrieves a hypermedia of data.
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves a hypermedia pagination of data.
         """
         data = self.get_page(page, page_size)
-        total_items = len(self.dataset)
+        total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
 
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return {
-            'page_size': page_size,
+            'page_size': len(data),
             'page': page,
             'data': data,
             'next_page': next_page,
