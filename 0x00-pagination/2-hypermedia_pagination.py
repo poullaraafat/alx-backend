ae785b4ad69bc114b2217464c7f512c379bb0b1b
paula raafat <paularaafat27@.com>
2024-10-28 22:03:07 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,19 +51,18 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Retrieves a hypermedia of data.
         """
-        data = self.get_page(page, page_size)  
-        total_items = len(self.dataset)  
-        total_pages = math.ceil(total_items / page_size)  
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset)
+        total_pages = math.ceil(total_items / page_size)
 
-        next_page = page + 1 if page < total_pages else None  
-        prev_page = page - 1 if page > 1 else None  
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
 
-        return {  
-            'page_size': page_size,  
-            'page': page,  
-            'data': data,  
-            'next_page': next_page,  
-            'prev_page': prev_page,  
-            'total_pages': total_pages,  
-        } 
-
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages,
+        }
