f1780defaac32bbbb2ae3db59a416d8bc34b3731
paula raafat <paularaafat27@.com>
2024-10-31 08:06:15 +0300

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -25,7 +25,7 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             first_item, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {first_item}")
 
